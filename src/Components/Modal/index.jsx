d8501f4726fe1e124fce501aa23107f1ca3a5fe9
Ingridy Rodrigues <ingridyrodrigues.f@hotmail.com>
2021-12-14 12:26:45 -0300

    merge: pull on modal

--- src/Components/Modal/index.jsx
+++ src/Components/Modal/index.jsx
@@ -1,36 +1,165 @@
-import { ModalContainer } from "./style";
-import { MdOutlineExitToApp } from "react-icons/md";
+import { IoIosArrowForward } from "react-icons/io";
+import { yupResolver } from "@hookform/resolvers/yup";
+/* import { Redirect } from "react-router"; */
+import { useForm } from "react-hook-form";
+import { ModalContainer } from "./style.js";
+import * as yup from "yup";
+import api from "../../Services/api";
 
 //usar o yup resolver para substituir o uso de states
 //passar o valor da category por props
 //passar uma props sinalizando o tipo do modal
+//titleModal
 
-const Modal = ({ title, text, /* type */ }) => {
+const Modal = ({ category = "workout", toggle, setToggle }) => {
+  
+
+
+  const schema = yup.object().shape({
+    title: yup.string(),
+    category: yup.string(),
+    frequency: yup.string().nullable().required("campo obrigatorio"),
+    difficulty: yup.string().nullable().required("campo obrigatorio"),
+    achieved: yup.string().default(false),
+    how_much_achieved: yup.string().default(120),
+    user: yup.string()
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmitFunction = ({
+    title,
+    category,
+    difficulty,
+    frequency
+  }) => {
+   
+    console.log(title);
+
+    api
+      .post("/habits", {title: title, category: category, difficulty: difficulty, frequency: frequency, achieved: false, how_much_achieved: 0, user: userId.user_id})
+      .then((_) => {
+        setToggle(false);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const functionTeste = () => {
+    console.log("oi")
+  }
+
   return (
-    /* {type === "habits" ? <ModalHabits></ModalHabits> : <ModalGoals></ModalGoals> */
     <ModalContainer>
-      <div className="modal-header">
-        <h1>{title}</h1>
-        <span>
-          <MdOutlineExitToApp />
-        </span>
-      </div>
-      <input type="text" placeholder={title} />
-      <p>How often?</p>
-      {/* essa  div será substituída pelo componente Button da branch components */}
-      <div className="frequency-container">
-        <button className="frequency">Daily</button>
-        <button className="frequency">Weekly</button>
-        <button className="frequency">Monthly</button>
-      </div>
-      <p>{text}</p>
-      {/* essa  div será substituída pelo componente Button da branch components */}
-      <div className="frequency-container">
-        <button className="frequency">Easy</button>
-        <button className="frequency">Hard</button>
-        <button className="frequency">Very Hard</button>
-      </div>
-      <button className="add">Add</button>
+      {/* <form onSubmit={handleSubmit(functionTeste)}>
+        <div className="modalHeader">
+          <h4>New Habit</h4> 
+          <span>
+            <IoIosArrowForward />
+          </span>
+        </div>
+        <input
+          className="addNewHabit"
+          type="text"
+          placeholder="add a new habit"
+          {...register("habit")}
+        />
+
+        <p>How often?</p>
+
+        <div className="frequencyContainer">
+          <div className="frequencyItem">
+            <label for="daily">
+              Daily
+              <input
+                type="radio"
+                className="frequency"
+                name="frequency"
+                id="daily"
+                value="daily"
+                {...register("frequency")}
+              />
+            </label>
+          </div>
+          <div className="frequencyItem">
+            <label for="weekly">
+              Weekly
+              <input
+                type="radio"
+                className="frequency"
+                name="frequency"
+                id="weekly"
+                value="weekly"
+                {...register("frequency")}
+              />
+            </label>
+          </div>
+          <div className="frequencyItem">
+            <label for="monthly">
+              Monthly
+              <input
+                type="radio"
+                className="frequency"
+                name="frequency"
+                id="monthly"
+                value="monthly"
+                {...register("frequency")}
+              />
+            </label>
+          </div>
+        </div>
+        <p>How is the difficulty?</p>
+        <div className="difficultyContainer">
+          <div className="difficultyItem">
+            <label>
+              Easy
+              <input
+                type="radio"
+                className="difficulty"
+                name="difficulty"
+                value="easy"
+                {...register("difficulty")}
+              />
+            </label>
+          </div>
+          <div className="difficultyItem">
+            <label>
+              Hard
+              <input
+                type="radio"
+                name="difficulty"
+                className="difficulty"
+                value="hard"
+                {...register("difficulty")}
+              />
+            </label>
+          </div>
+          <div className="difficultyItem">
+            <label>
+              Very Hard
+              <input
+                type="radio"
+                name="difficulty"
+                className="difficulty"
+                value="very_hard"
+                {...register("difficulty")}
+              />
+            </label>
+          </div>
+        </div>
+        <button type="submit" className="buttonModal">
+          Add
+        </button>
+      </form> */}
+      <form onSubmit={handleSubmit(functionTeste)}> 
+        <input name="title" {...register("title")}/>
+        <button type="submit">add</button>
+      </form>
     </ModalContainer>
   );
 };
--- src/Components/Modal/style.js
+++ src/Components/Modal/style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const ModalContainer = styled.div`
-  width: 270px;
+  width: 300px;
   height: 330px;
   border: solid var(--grey-50) 1px;
   border-radius: 10px;
@@ -11,81 +11,53 @@ export const ModalContainer = styled.div`
   margin-top: 30px;
   padding: 12px;
 
-  @media (min-width: 768px) {
-    width: 560px;
-  }
-
-  h1 {
-    font-size: 1.5rem;
-  }
-
-  svg {
-    width: 30px;
-    height: 30px;
-  }
-
-  .modal-header {
-    min-width: 240px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-  }
-
-  .frequency-container {
-    margin: 0 auto;
-    min-width: 230px;
+  .modalHeader {
     display: flex;
     justify-content: space-between;
-    margin-top: 10px;
-
-    @media (min-width: 768px) {
-      width: 530px;
-    }
   }
 
-  .frequency {
-    min-width: 73px;
-    height: 40px;
-    background-color: var(--red);
+  .addNewHabit {
+    margin: 10px 0;
+    width: 100%;
+    padding: 5px;
     border-radius: 5px;
-    color: var(--white);
+    border: 1px solid var(--grey-50);
+  }
+
+  .buttonModal {
+    width: 100%;
+    background-color: red; /* trocar por props */
+    padding: 10px;
+    border-radius: 5px;
+    color: white;
+  }
+
+  /* .frequency,
+  .difficulty {
+    opacity: 0;
+    position: absolute;
+    width: 0;
+  } */
+
+  .frequencyContainer,
+  .difficultyContainer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+  }
+
+  .difficultyItem,
+  .frequencyItem {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: red; /* trocar por props */
+    margin: 10px 0;
+    padding: 10px;
+    border-radius: 5px;
+    width: 90px;
     font-size: 14px;
-
-    @media (min-width: 768px) {
-      width: 150px;
-    }
-  }
-
-  input {
-    min-width: 230px;
-    height: 40px;
-    border-radius: 5px;
-    border: solid var(--grey-50) 1px;
-    margin: 0 auto;
-    font-size: 1.2rem;
-    margin-top: 10px;
-    margin-bottom: 10px;
-
-    @media (min-width: 768px) {
-      width: 530px;
-    }
-  }
-
-  p {
-    margin-top: 10px;
-  }
-
-  .add {
-    min-width: 230px;
-    height: 40px;
-    background-color: var(--red);
-    color: var(--white);
-    border-radius: 5px;
-    margin: 0 auto;
-    margin-top: 10px;
-
-    @media (min-width: 768px) {
-      width: 530px;
-    }
+    color: white;
   }
 `;

d73987ddd030bd5ca3759daebc9977cad3d6de06
Débora Gama <gama.debora1@gmail.com>
2021-12-15 20:56:28 -0300

    list render and map

--- src/Pages/Goals/index.jsx
+++ src/Pages/Goals/index.jsx
@@ -2,12 +2,32 @@ import Header from "../../Components/Header";
 import Modal from "../../Components/ModalGoals/index";
 import imgGoals from "../../assets/img/image-goals/Personal goals-amico 1.png";
 import { GoalsContainer } from "./style";
+import ItemList from "../../Components/ItemList";
+import { GoalsContext } from "../../Provider/goals/goals";
+import { useContext } from "react";
+import { MdOutlineLteMobiledata } from "react-icons/md";
 
 const Goals = (setToggle) => {
+  const { goalsList } = useContext(GoalsContext);
+
+  console.log(goalsList);
   return (
     <>
       <Header backgroundColor={"var(--orange)"} />
       <GoalsContainer>
+        <ul>
+          {goalsList.map((item) => (
+            <ItemList
+              key={item.id}
+              color={"var(--orange)"}
+              name={item.title}
+              requirementTitle={"Status"}
+              difficultyValue={item.difficulty}
+              requirementValue={item.achieved}
+            />
+          ))}
+        </ul>
+
         <div className="modal-container">MODAL</div>
         <div className="img-container">
           <img src={imgGoals} alt="Goals" />

c7e90f7e780bc1a143e8fc8de07b7ee5cb49823f
Ingridy Rodrigues <ingridyrodrigues.f@hotmail.com>
2021-12-16 19:56:33 -0300

    merge: habits on develop

--- src/Components/ModalHabits/index.jsx
+++ src/Components/ModalHabits/index.jsx
@@ -13,8 +13,6 @@ const ModalHabits = ({ category, setToggle, setToggleMenu, setToggleList }) => {
   const { habitsControl, setHabitsControl } = useContext(HabitsContext);
   const userId = window.localStorage.getItem("id");
 
-  console.log(authorization);
-
   const schema = yup.object().shape({
     title: yup.string(),
     difficulty: yup.string(),

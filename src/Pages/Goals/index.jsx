01ee98e37ea4637214ef92e96ce20e9ff254cad4
Débora Gama <gama.debora1@gmail.com>
2021-12-15 23:55:14 -0300

    style: goals nd goals modal pages

--- src/Pages/Goals/index.jsx
+++ src/Pages/Goals/index.jsx
@@ -5,6 +5,7 @@ import { GoalsContext } from "../../Provider/goals/goals";
 import { useContext, useEffect, useState } from "react";
 import Button from "../../Components/Button/index";
 import vector from "../../assets/img/image-goals/Vector 1.png";
+import Modal from "../../Components/ModalGoals/index";
 
 const Goals = ({ history }) => {
   const { goalsList } = useContext(GoalsContext);
@@ -27,34 +28,37 @@ const Goals = ({ history }) => {
   return (
     <>
       <Header backgroundColor={"var(--orange)"} />
+
       <GoalsContainer>
-        <h1>Goals</h1>
-        <ul>
-          {goalsList.map((item) => (
-            <ItemList
-              key={item.id}
-              color={"var(--orange)"}
-              name={item.title}
-              requirementTitle={"Status"}
-              difficultyValue={item.difficulty}
-              requirementValue={item.achieved}
-            />
-          ))}
-        </ul>
-        {toggle ? (
-          <Button
-            onClick={onClick}
-            width={`${322}px`}
-            // backgroundHover={backgroundHover}
-            // width={"200px"}
-            // margin={margin}
-            // type={type}
-          />
-        ) : (
-          <Button onClick={addGoal} />
-        )}
+        {/* <div className="modal-container">
+          <Modal />
+        </div> */}
+        <div className="list-container">
+          <h1>Goals</h1>
+          <ul>
+            {goalsList.map((item) => (
+              <ItemList
+                key={item.id}
+                color={"var(--orange)"}
+                name={item.title}
+                requirementTitle={"Status"}
+                difficultyValue={item.difficulty}
+                requirementValue={item.achieved}
+              />
+            ))}
+          </ul>
+        </div>
       </GoalsContainer>
-      <img src={vector} alt="goals-footer" />
+      {toggle ? (
+        <>
+          <Button onClick={onClick} width={`${322}px`} />;
+          <img className="footer" src={vector} alt="goals-footer" />;
+        </>
+      ) : (
+        <>
+          <Button onClick={addGoal} />
+        </>
+      )}
     </>
   );
 };
--- src/Pages/Goals/style.js
+++ src/Pages/Goals/style.js
@@ -5,16 +5,41 @@ export const GoalsContainer = styled.div`
   flex-direction: column;
   align-items: center;
 
+  @media (min-width: 900px) {
+    flex-direction: row;
+    align-items: center;
+
+    .footer {
+      display: none;
+    }
+  }
+
   h1 {
     color: var(--orange);
     margin: 10px;
   }
+  .modal-container {
+    width: 500px;
+    border: solid blie 2px;
+  }
+
+  .list-container {
+   
+
+    @media(min:900px){
+      width:50vw;
+    }
+
+  }
 
   ul {
-    background-color: #fdf0f0;
+    background-color: #f5f5f5;
     padding: 10px;
     border-radius: 10px;
     margin-bottom: 20px;
+  }
+
+
   }
   img {
     width: 250px;

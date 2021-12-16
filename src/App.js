18424487c5b8c086d87b9d6782c9f3efc79df0f7
Débora Gama <gama.debora1@gmail.com>
2021-12-16 17:45:34 -0300

    merge: pull develop to dashboard-goals-a

--- src/App.js
+++ src/App.js
@@ -3,38 +3,26 @@ import { ActivitiesProvider } from "./Provider/Activities/activities";
 import { DashboardProvider } from "./Provider/dashboard/dashboard";
 import { HabitsProvider } from "./Provider/Habits/habits";
 import { GroupsProvider } from "./Provider/Groups/groups";
-<<<<<<< HEAD
 import { GoalsProvider } from "./Provider/goals/goals";
-=======
 import { LoginProvider } from "./Provider/Login/Login";
 
 import Routes from "./Routes";
->>>>>>> 1f8338db884a76972bb065d3d9021bf1d46cf4d0
 function App() {
   return (
     <>
       {/*<ExemploDeProvider>*/}
       <LoginProvider>
         <GroupsProvider>
-<<<<<<< HEAD
-          <DashboardProvider>
-            <ActivitiesProvider>
-              <GoalsProvider>
-                <GlobalStyle />
-                <Routes />
-              </GoalsProvider>
-            </ActivitiesProvider>
-          </DashboardProvider>
-=======
           <HabitsProvider>
             <DashboardProvider>
-              <ActivitiesProvider>
-                <GlobalStyle />
-                <Routes />
-              </ActivitiesProvider>
+              <GoalsProvider>
+                <ActivitiesProvider>
+                  <GlobalStyle />
+                  <Routes />
+                </ActivitiesProvider>
+              </GoalsProvider>
             </DashboardProvider>
           </HabitsProvider>
->>>>>>> 1f8338db884a76972bb065d3d9021bf1d46cf4d0
         </GroupsProvider>
       </LoginProvider>
       {/*</ExemploDeProvider>*/}
